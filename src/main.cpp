ecbf7185d56d58ad49db2823ab3eda40ba12ce5a
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-03-04 19:14:12 +0100

    Fix libqmlparticlesplugin load path

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@
 int main(int argc, char *argv[]) {
 	QApplication app(argc, argv);
 
-    qmlRegisterType<TileData>();
-    QDeclarativeView viewer;
-    Core* game = new Core;
-    viewer.engine()->rootContext()->setContextObject(game);
-	viewer.engine()->importPlugin("/accounts/1000/appdata/com.mpl.minesweeper.testDev_minesweeper964f3f34/app/native/lib/qml/libqmlparticlesplugin.so", "Qt.labs.particles", NULL); 
-    viewer.setSource(QUrl("qrc:/qml/main.qml"));
-    viewer.setWindowIcon(QPixmap(":/res/mine"));
-    viewer.show();
+	qmlRegisterType<TileData>();
+	QDeclarativeView viewer;
+	Core* game = new Core;
+	viewer.engine()->rootContext()->setContextObject(game);
+	viewer.engine()->importPlugin("app/native/lib/qml/libqmlparticlesplugin.so", "Qt.labs.particles", NULL);
+	viewer.setSource(QUrl("qrc:/qml/main.qml"));
+	viewer.setWindowIcon(QPixmap(":/res/mine"));
+	viewer.show();
 
-    return app.exec();
+	return app.exec();
 }
 
