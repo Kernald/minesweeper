56662866c41312d9b03d47896312853dc704b13a
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-03-05 02:11:24 +0100

    Indent

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,12 @@ int main(int argc, char *argv[]) {
 	QDeclarativeView viewer;
 	Core* game = new Core;
 	viewer.engine()->rootContext()->setContextObject(game);
-    viewer.engine()->rootContext()->setContextProperty("core", game);
+	viewer.engine()->rootContext()->setContextProperty("core", game);
 #ifdef __QNX__
 	viewer.engine()->importPlugin("app/native/lib/qml/libqmlparticlesplugin.so", "Qt.labs.particles", NULL);
 #endif // __QNX__
 #ifdef _MSC_VER
-    viewer.engine()->importPlugin(QDir().absolutePath() + "/qmlparticlesplugin.dll", "Qt.labs.particles", NULL);
+	viewer.engine()->importPlugin(QDir().absolutePath() + "/qmlparticlesplugin.dll", "Qt.labs.particles", NULL);
 #endif // _MSC_VER
 	viewer.setSource(QUrl("qrc:/qml/main.qml"));
 	viewer.setWindowIcon(QPixmap(":/res/mine"));
